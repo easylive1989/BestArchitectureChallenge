328020bca3d53e4080549b4b8f612c8d0052dd4e
Paul <easylive1989@gmail.com>
2021-08-22 09:30:47 +0800

    move test/bloc folder out of domain

--- test/bloc/post_cubit_test.dart
+++ test/bloc/post_cubit_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../fixture.dart';
+import '../fixture.dart';
 import 'post_cubit_test.mocks.dart';
 
 late MockPostService mockPostService;
